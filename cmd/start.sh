4ce372da350a2bdb4604c7a2123bfe5d2668125d
“zepen” <18217127859@qq.com>
2024-10-21 11:35:36 +0800

    update

--- cmd/start.sh
+++ cmd/start.sh
@@ -7,12 +7,10 @@ if [ ! -n "$env" ];then
 fi
 export REC_ENV="$env"
 git pull origin main
-git config --global http.extraheader "PRIVATE-TOKEN:glpat-yysVywQqx27fpCTM6Zcc"
-git config --global url."git@gitlab.com:cher8/lion.git".insteadOf "https://gitlab.com/cher8/lion.git"
 #chmod 600 ~/.ssh/id_rsa.pub && eval "$(ssh-agent -s)"
 #ssh-add ~/.ssh/id_rsa.pub
 export GOPROXY=https://goproxy.cn
-export GOPRIVATE=gitlab.com/cher8
+export GOPRIVATE=
 echo "GOPROXY="$GOPROXY
 echo "GOPRIVATE="$GOPRIVATE
 echo "generate protobuf code..."
